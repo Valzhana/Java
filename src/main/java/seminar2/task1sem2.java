46df623caf87656168c9ae01a2408a32fc76df00
Olga Gladkaya <tarantasik@mail.ru>
2023-01-30 19:54:54 +0300

    added task1sem2

--- src/main/java/seminar2/task1sem2.java
+++ src/main/java/seminar2/task1sem2.java
@@ -1,4 +1,16 @@
 package seminar2;
 
 public class task1sem2 {
+    public static void main(String[] args) {
+        isTheStringPalindrome("racecar");
+    }
+    private static void isTheStringPalindrome(String str) {
+        int length = str.length();
+        Boolean result = null;
+        for (int i = 0; i < (length/2); i++) {
+            result = (str.toLowerCase().charAt(i) == str.toLowerCase().charAt(length - i - 1));
+
+        }
+        System.out.println(result);
+    }
 }
