547da7e64c60e5c8bc7c1547d4839b158d8976b9
Olga Gladkaya <tarantasik@mail.ru>
2023-02-25 05:50:19 +0300

    added task1Sem5

--- src/main/java/seminar5/task1.java
+++ src/main/java/seminar5/task1.java
@@ -1,14 +1,16 @@
 package seminar5;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 
 public class task1 {
     public static void main(String[] args) {
-        createPhoneBook();
+        getNumberByName(createPhoneBook());
+        getNameByNumber(createPhoneBook());
     }
-    private static void createPhoneBook() {
+    private static HashMap createPhoneBook() {
         HashMap<String, String> phoneBook = new HashMap<>();
         phoneBook.put("8 927 015-82-47", "Belov Alexey Ivanovich");
         phoneBook.put("8 927 075-82-07", "Nikolaev Ivan Vladimirovich");
@@ -21,20 +23,47 @@ public class task1 {
         phoneBook.put("8 986 057-82-03", "Egorova Elena Maximovna");
         phoneBook.put("8 505 185-98-31", "Malina Anna Vadimovna");
         phoneBook.put("8 920 015-92-02", "Nikolaev Ivan Vladimirovich");
-
+        return phoneBook;
+    }
+    private static void getNumberByName(Map<String, String> phoneBook) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Input the name, please: ");
-        String name = in.nextLine();
-
-        for (String key : phoneBook.keySet()) {
-            if (name.equals(phoneBook.get(key))) {
-                System.out.printf("%s, phone number: %s\n", phoneBook.get(key), key);
+        String name = "";
+        do {
+            System.out.print("Input the name, please: ");
+            try {
+                name = in.nextLine();
+            } catch (Exception e) {
+                System.out.println("Error");
+                e.printStackTrace();
+            }
+            for (String key : phoneBook.keySet()) {
+                if (name.equals(phoneBook.get(key))) {
+                    System.out.printf("%s, phone number: %s\n", phoneBook.get(key), key);
+                }
             }
         }
-
-        System.out.print("Input the phone number, please: ");
-        String number = in.nextLine();
+        while (phoneBook.containsValue(name));
+        System.out.println("There is no such contact in the phonebook!");
+    }
+    private static void getNameByNumber(Map<String, String> phoneBook) {
+        Scanner in = new Scanner(System.in);
+        String number = "";
+        do {
+            System.out.print("Input the number, please: ");
+            try {
+                number = in.nextLine();
+            } catch (Exception e) {
+                System.out.println("Error");
+                e.printStackTrace();
+            }
+            for (String key : phoneBook.keySet()) {
+                if (phoneBook.get(key).equals(phoneBook.get(number))){
+                    System.out.printf("%s, phone number: %s\n", phoneBook.get(key), key);
+                }
+            }
+        }
+        while (phoneBook.containsKey(number));
+        System.out.println("There is no such contact in the phonebook!");
         in.close();
-        System.out.printf("%s, phone number: %s\n", phoneBook.get(number), number);
     }
 }
